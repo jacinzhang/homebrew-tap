1d6e1ab0a6929744cc96d0e56563606b19fb2f4f
jacinzhang <270611346@qq.com>
2024-01-20 15:26:09 +0800

    mycli 0.1.7
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mycli.rb
+++ Formula/mycli.rb
@@ -1,9 +1,9 @@
 class Mycli < Formula
   desc "CLI"
   homepage "https://github.com/jacinzhang/MyCLI"
-  url "https://github.com/jacinzhang/MyCLI/releases/download/0.1.3/0.1.3.tar.gz"
-  version "0.1.3"
-  sha256 "0f440126cab9d3c7f6d1b01e2b80e610bb2f6ed4754141cff71981f315a2442f"
+  url "https://github.com/jacinzhang/MyCLI/releases/download/0.1.7/0.1.7.tar.gz"
+  version "0.1.7"
+  sha256 "6502c461d6e8547c5566ebd1d5f48745a26f17e06ebde6094efaebbf6fa5fc2d"
   license ""
 
   def install
