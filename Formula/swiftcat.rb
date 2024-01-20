24ef2172a2804c93c2e8e789569170c4ca1fcd4e
jacinzhang <270611346@qq.com>
2024-01-21 00:14:48 +0800

    swiftcat 0.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/swiftcat.rb
+++ Formula/swiftcat.rb
@@ -1,9 +1,9 @@
 class Swiftcat < Formula
   desc "CLI"
   homepage "https://github.com/jacinzhang/swiftcat"
-  url "https://github.com/jacinzhang/swiftcat/releases/download/0.0.2/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "614d72ca1a2961db299e81ba2bf41b36b9f0e78227ff045e87b708db5496f52e"
+  url "https://github.com/jacinzhang/swiftcat/releases/download/0.0.3/0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 "c2b6ac840c0c928d41bf86528f9bb0ece89f9e9529f53c1dab894db55592612f"
   license ""
 
   def install
